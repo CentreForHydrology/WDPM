b06c341597de68772dd9470a49bc8911b30395d9
Kevin Shook <kevin.shook@usask.ca>
2020-06-06 21:33:44 -0600

    added missing files cmap_black.sh and colormap_black.txt

---
+++ src/cmap_black.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+startingDir=`pwd`
+# creates color map from water applied to DEM
+infile=$1
+outfile="${infile%.*}"
+gdaldem color-relief $infile $startingDir/colormap_black.txt -OF png $outfile".png"
+# delete superfluous xml file
+rm $outfile".png.aux.xml"
