01d10e3d6f22317733d6b934f650eba1ff151004
Kevin Shook <kevin.shook@usask.ca>
2021-02-02 20:33:19 -0600

    updated examples in manual, cmap_black.sh now allows spaces in path

--- src/cmap_black.sh
+++ src/cmap_black.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-startingDir=`pwd`
+startingDir=$PWD
+# remove spaces from path
+
 # creates color map from water applied to DEM
 infile=$1
 outfile="${infile%.*}"
-gdaldem color-relief $infile $startingDir/colormap_black.txt -OF png $outfile".png"
+gdaldem color-relief $infile "$startingDir"/colormap_black.txt -OF png $outfile".png"
 # delete superfluous xml file
 rm $outfile".png.aux.xml"
